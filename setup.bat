f983b911253b5b4a771900df39bcc128edc6c5cc
i-saint <saint.skr@gmail.com>
2016-04-03 19:52:38 +0900

    DWANGO_Plugins.7z

--- setup.bat
+++ setup.bat
@@ -2,3 +2,6 @@
 
 cd Plugin\External
 7z\7za.exe x -aos *.7z
+
+cd ..\..\OpenToonzPluginForUnity\Assets\StreamingAssets\OpenToonzPlugin
+..\..\..\..\Plugin\External\7z\7za.exe x -aos ..\..\..\..\Packages\DWANGO_Plugins.7z
